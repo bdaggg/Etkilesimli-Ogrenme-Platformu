37030f5045d3252e4659b46b5e3585d3fdfef603
bulent dag <bulent.dag223@outlook.com>
2024-03-03 22:14:12 +0300

    teachquess için crudlar yazildi

--- controllers/teacherController.go
+++ controllers/teacherController.go
@@ -4,7 +4,6 @@ import (
 	"EOP/database"
 	"EOP/middleware"
 	"EOP/model"
-	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -15,15 +14,48 @@ func AddQuestions(c *fiber.Ctx) error {
 		return c.Status(500).JSON(fiber.Map{"status": "error", "data": err})
 	}
 	var Exam model.TeacherQuestion
-	log.Println("buraya geldi -1 ----------------")
 	c.BodyParser(&Exam)
 	Exam.UserID = user.UserID
-	log.Println("buraya geldi -2 ----------------")
 	err = database.DB.Db.Save(Exam).Error
 	if err != nil {
-		log.Println("buraya geldi -3 ----------------")
 		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "database error", "data": err})
 	}
-	log.Println("buraya geldi -4 ----------------")
 	return c.Status(200).JSON(fiber.Map{"status": "success", "message": "exam added success", "data": Exam})
+}
+
+func UpdateQuestions(c *fiber.Ctx) error {
+	_, err := middleware.TokenControl(c)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"status": "error", "data": err})
+	}
+	var UpdateQuess model.TeacherQuestion
+	//var OldQuess model.TeacherQuestion
+	c.BodyParser(&UpdateQuess)
+	err = database.DB.Db.Model(model.TeacherQuestion{}).Where("id = ?", UpdateQuess.ID).Updates(model.TeacherQuestion{Question: UpdateQuess.Question, Answer1: UpdateQuess.Answer1,
+		Answer2: UpdateQuess.Answer2, Answer3: UpdateQuess.Answer3, Answer4: UpdateQuess.Answer4, TrueAnswer: UpdateQuess.TrueAnswer}).Error
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"status": "error", "data": err})
+	}
+
+	return c.Status(200).JSON(fiber.Map{"status": "success", "data": UpdateQuess})
+
+}
+
+func DeleteQuestions(c *fiber.Ctx) error {
+	_, err := middleware.TokenControl(c)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"status": "error", "data": err})
+	}
+	id := c.BodyParser("id")
+	var quess model.TeacherQuestion
+	err = database.DB.Db.Where("id=?", id).Find(&quess).Error
+	if err != nil {
+		return err
+	}
+	err = database.DB.Db.Delete(&quess).Error
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{"status": "delete success"})
 }
